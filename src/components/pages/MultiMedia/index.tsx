91c2d87958fff5739b194e6530c28d782f20cfe1
Marcelo <marcelo@streetwire.net>
2024-09-03 13:01:05 -0400

    "Added two YouTube iframes to MultiMedia component"

--- src/components/pages/MultiMedia/index.tsx
+++ src/components/pages/MultiMedia/index.tsx
@@ -1,7 +1,7 @@
 import Layout from '@/components/Layout'
+import * as S from '@/components/pages/StepByStep/style'
 import useAppContext from '@/hooks/useAppContext'
 import * as SG from '@/styles/global'
-import * as S from '@/components/pages/StepByStep/style'
 
 export default function MultiMedia() {
   const { state } = useAppContext()
@@ -13,6 +13,19 @@ export default function MultiMedia() {
         <S.WrapperText>
           <SG.Text>{multimedia.paragraphy_01}</SG.Text>
         </S.WrapperText>
+        <div style={{display: 'flex'}}>
+          <iframe width="100%" height="480" 
+            src="https://www.youtube.com/embed/ShDu_8x-R2c" 
+            title="Calculadora de Impactos de la Minería Ilegal de Oro: Entrevista a Fabiano Fernandes (MPF, Brasil)" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
+          </iframe>
+          <iframe width="100%" height="480" 
+            src="https://www.youtube.com/embed/Ag29apNrziA" 
+            title="Resumen: Calculadora de Impactos de la Minería Ilegal de Oro en la Amazonía" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
+          </iframe>
+        </div>
+        
       </SG.Container>
     </Layout>
   )
