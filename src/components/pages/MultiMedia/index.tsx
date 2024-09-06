1d3a8d3ed84de9e1771f9173ebd6a8be184221e0
Marcelo <marcelo@streetwire.net>
2024-09-06 14:23:12 -0400

    "Replace iframes with custom video thumbnails and links"

--- src/components/pages/MultiMedia/index.tsx
+++ src/components/pages/MultiMedia/index.tsx
@@ -14,16 +14,30 @@ export default function MultiMedia() {
           <SG.Text>{multimedia.paragraphy_01}</SG.Text>
         </S.WrapperText>
         <div style={{display: 'flex'}}>
-          <iframe width="100%" height="480" 
-            src="https://www.youtube.com/embed/ShDu_8x-R2c" 
-            title="Calculadora de Impactos de la Minería Ilegal de Oro: Entrevista a Fabiano Fernandes (MPF, Brasil)" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
-          </iframe>
-          <iframe width="100%" height="480" 
-            src="https://www.youtube.com/embed/Ag29apNrziA" 
-            title="Resumen: Calculadora de Impactos de la Minería Ilegal de Oro en la Amazonía" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
-          </iframe>
+          <div style={{padding: '20px', border: '1px solid', borderRadius: '15px', marginRight: '20px'}}>
+            <a href='https://www.youtube.com/watch?v=ShDu_8x-R2c' target='_blank'>
+              <img src='/assets/images/video-1.jpg'></img>
+            </a>
+            <div style={{display: 'flex', justifyContent: 'space-between', marginBottom: '20px'}}>
+              <span>5:08</span>
+              <span>12 de jul. de 2023</span>
+            </div>
+            <h2>
+              Calculadora de Impactos de la Minería Ilegal de Oro: Entrevista a Fabiano Fernandes (MPF, Brasil)
+            </h2>
+          </div>
+          <div style={{padding: '20px', border: '1px solid', borderRadius: '15px'}}>
+            <a href='https://www.youtube.com/watch?v=Ag29apNrziA' target='_blank'>
+              <img src='/assets/images/video-2.jpg'></img>
+            </a>
+            <div style={{display: 'flex', justifyContent: 'space-between', marginBottom: '20px'}}>
+              <span>7:47</span>
+              <span>4 de jan. de 2024</span>
+            </div>
+            <h2>
+              Calculadora de Impactos de la Minería Ilegal de Oro en la Amazonía
+            </h2>
+          </div>          
         </div>
         
       </SG.Container>
