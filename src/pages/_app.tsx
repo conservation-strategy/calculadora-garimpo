85f64f22a9c4198b196d4bde2875810f63b5ef41
Eric Hildebrand <eric@blackantelope.com>
2024-10-02 12:36:34 -0700

    Add trackPageViews parameter to GoogleAnalytics Component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,7 +27,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Script type="text/javascript" id="lgpd">
           {`(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);`}
         </Script>
-        <GoogleAnalytics />
+        <GoogleAnalytics trackPageViews />
         <Component {...pageProps} />
         <Analytics />
       </AppContextProvider>
